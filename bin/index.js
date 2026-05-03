0d85d7c9a4dcd6639e8cf9fe5cd6e4a183836d6b
SACHITH <kasasachith@gmail.com>
2026-05-03 08:44:09 +0530

    successfully creates the client and server folders

--- bin/index.js
+++ bin/index.js
@@ -4,4 +4,35 @@
 // This is called a shebang.
 // It helps when your CLI is installed globally later.
 
-console.log('CLI is working.')
+const inquirer = require("inquirer").default
+const fs = require("fs")
+const path = require("path")
+
+inquirer
+  .prompt([
+    {
+      name: "projectName",
+      message: "Enter your project name:"
+    },
+    {
+      name: "frontend",
+      message: "Choose frontend:",
+      type: "list",
+      choices: ["React", "HTML"]
+    },
+    {
+      name: "backend",
+      message: "Choose backend:",
+      type: "list",
+      choices: ["Express", "None"]
+    }
+  ])
+  .then((answers) => {
+    const projectPath = path.join(process.cwd(), answers.projectName)
+
+    fs.mkdirSync(projectPath)
+    fs.mkdirSync(path.join(projectPath, "client"))
+    fs.mkdirSync(path.join(projectPath, "server"))
+
+    console.log("\nProject folder created successfully!")
+  })

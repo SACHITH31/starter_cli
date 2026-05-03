54ee84ad8874b7eedb4d0d058645ea7a289be200
SACHITH <kasasachith@gmail.com>
2026-05-03 10:15:47 +0530

    shows the error message when the folder is existing

--- bin/index.js
+++ bin/index.js
@@ -1,68 +1,85 @@
 #!/usr/bin/env node
 
-
 // This is called a shebang.
 // It helps when your CLI is installed globally later.
 
-const inquirer = require("inquirer").default
-const fs = require("fs")
-const path = require("path")
+const inquirer = require("inquirer").default;
+const fs = require("fs");
+const path = require("path");
 
 inquirer
   .prompt([
     {
       name: "projectName",
-      message: "Enter your project name:"
+      message: "Enter your project name:",
     },
     {
       name: "frontend",
       message: "Choose frontend:",
       type: "list",
-      choices: ["React", "HTML"]
+      choices: ["None", "React", "HTML"],
+      default: "None",
     },
     {
       name: "backend",
       message: "Choose backend:",
       type: "list",
-      choices: ["Express", "None"]
-    }
+      choices: ["None", "Node", "Express"],
+      default: "None",
+    },
   ])
   .then((answers) => {
-  const projectPath = path.join(process.cwd(), answers.projectName)
+    const projectPath = path.join(process.cwd(), answers.projectName);
+    const backend = answers.backend.toLowerCase();
 
-  fs.mkdirSync(projectPath)
-  fs.mkdirSync(path.join(projectPath, "client"))
-  fs.mkdirSync(path.join(projectPath, "server"))
+    if (fs.existsSync(projectPath)) {
+      console.log("\nA project with this name already exists.");
+      return;
+    }
 
-  fs.writeFileSync(
-    path.join(projectPath, ".gitignore"),
-    "node_modules\n.env"
-  )
+    fs.mkdirSync(projectPath);
 
-  fs.writeFileSync(
-    path.join(projectPath, "README.md"),
-    `# ${answers.projectName}`
-  )
+    if (answers.frontend !== "None") {
+      fs.mkdirSync(path.join(projectPath, "client"));
+    }
 
-  const serverTemplate = fs.readFileSync(
-  path.join(process.cwd(), "templates", "server", "index.js"),
-  "utf-8"
-)
+    if (backend !== "none") {
+      fs.mkdirSync(path.join(projectPath, "server"));
+    }
 
-const clientTemplate = fs.readFileSync(
-  path.join(process.cwd(), "templates", "client", "package.json"),
-  "utf-8"
-)
+    fs.writeFileSync(
+      path.join(projectPath, ".gitignore"),
+      "node_modules\n.env",
+    );
 
-fs.writeFileSync(
-  path.join(projectPath, "server", "index.js"),
-  serverTemplate
-)
+    fs.writeFileSync(
+      path.join(projectPath, "README.md"),
+      `# ${answers.projectName}`,
+    );
 
-fs.writeFileSync(
-  path.join(projectPath, "client", "package.json"),
-  clientTemplate
-)
+    if (answers.frontend !== "None") {
+      const clientTemplate = fs.readFileSync(
+        path.join(process.cwd(), "templates", "client", "package.json"),
+        "utf-8",
+      );
 
-  console.log("\nProject created successfully!")
-})
+      fs.writeFileSync(
+        path.join(projectPath, "client", "package.json"),
+        clientTemplate,
+      );
+    }
+
+    if (backend !== "none") {
+      const serverTemplate = fs.readFileSync(
+        path.join(process.cwd(), "templates", "server", "index.js"),
+        "utf-8",
+      );
+
+      fs.writeFileSync(
+        path.join(projectPath, "server", "index.js"),
+        serverTemplate,
+      );
+    }
+
+    console.log("\nProject created successfully!");
+  });
